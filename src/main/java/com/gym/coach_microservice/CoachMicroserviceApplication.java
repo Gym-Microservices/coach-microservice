c9237abd8f6a0dcbf99f3f890fe1b0e307e798db
MGMaikeru <13miguegonza2005@gmail.com>
2025-08-11 20:06:17 -0500

    initial commit

---
+++ src/main/java/com/gym/coach_microservice/CoachMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gym.coach_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/coach_microservice/CoachMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.coach_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
