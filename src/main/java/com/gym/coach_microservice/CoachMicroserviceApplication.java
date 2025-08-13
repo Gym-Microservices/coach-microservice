de7e797a885c0f0f9de89a1d04bb9975ff9007b8
MGMaikeru <13miguegonza2005@gmail.com>
2025-08-13 14:02:26 -0500

    chore: add eureka service discovery

--- src/main/java/com/gym/coach_microservice/CoachMicroserviceApplication.java
+++ src/main/java/com/gym/coach_microservice/CoachMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.gym.coach_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CoachMicroserviceApplication {
 
 	public static void main(String[] args) {
